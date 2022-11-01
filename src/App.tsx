e63c591b7bfb393978ed31ce3558962a5a513e5f
Danilo Alves <dannalves111@gmail.com>
2022-10-31 22:57:27 -0300

    add: notification component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,114 @@
+import { useState } from 'react';
 import './App.less';
+import { Notification, NotificationProps } from './components/Notification';
+
+const Notifications: NotificationProps[] = [
+  {
+    user: 'Mark Webber',
+    userPicture: '/avatar-mark-webber.webp',
+    message: 'reacted to your recent post',
+    time: '1m ago',
+    unread: true,
+    link: 'My first tournament today!',
+  },
+  {
+    user: 'Angela Gray',
+    userPicture: '/avatar-angela-gray.webp',
+    message: 'followed you',
+    time: '5m ago',
+    unread: true,
+  },
+  {
+    user: 'Jacob Thompson',
+    userPicture: '/avatar-jacob-thompson.webp',
+    message: 'has joined your group',
+    time: '1 day ago',
+    unread: true,
+    link: 'Chess Club',
+  },
+  {
+    user: 'Rizky Hasanuddin',
+    userPicture: '/avatar-rizky-hasanuddin.webp',
+    message: 'sent you a private message',
+    time: '5 days ago',
+    unread: false,
+    privateMessage: `Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
+    I'm already having lots of fun and improving my game.`,
+  },
+  {
+    user: 'Kimberly Smith',
+    userPicture: '/avatar-kimberly-smith.webp',
+    message: 'commented on your picture',
+    time: '1 week ago',
+    unread: false,
+    picture: '/image-chess.webp',
+  },
+  {
+    user: 'Nathan Peterson',
+    userPicture: '/avatar-nathan-peterson.webp',
+    message: 'reacted to your recent post',
+    time: '2 weeks ago',
+    unread: false,
+    link: '5 end-game strategies to increase your win rate',
+  },
+  {
+    user: 'Anna Kim',
+    userPicture: '/avatar-anna-kim.webp',
+    message: 'left the group',
+    time: '2 weeks ago',
+    unread: false,
+    link: 'Chess Club',
+  },
+];
 
 function App() {
-  return <>Hello world!</>;
+  const [notifications, setNotifications] = useState(Notifications);
+
+  function toggleRead() {
+    setNotifications(
+      notifications.map((notification) => {
+        return { ...notification, unread: !notification.unread };
+      })
+    );
+  }
+
+  function markAllAsRead() {
+    setNotifications(
+      notifications.map((notification) => ({ ...notification, unread: false }))
+    );
+  }
+
+  return (
+    <div className="container">
+      <header>
+        <h1>
+          Notifications
+          <span className="counter">
+            {
+              notifications.filter((notification) => {
+                return notification.unread;
+              }).length
+            }
+          </span>
+        </h1>
+        <aside>
+          <button onClick={markAllAsRead}>Mark all as read</button>
+        </aside>
+      </header>
+      <main>
+        <ul>
+          {notifications.map((notification) => {
+            return (
+              <Notification
+                key={notification.user}
+                {...notification}
+              ></Notification>
+            );
+          })}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Notification.tsx
@@ -0,0 +1,32 @@
+export interface NotificationProps {
+  user: string;
+  userPicture: string;
+  message: string;
+  time: string;
+  unread: boolean;
+  link?: string;
+  picture?: string;
+  privateMessage?: string;
+  onClick: () => void;
+}
+
+export function Notification(props: NotificationProps) {
+  const { user, userPicture, message, time, unread } = props;
+  return (
+    <li className={unread ? 'unread' : ''}>
+      <img src={userPicture} alt={user + ' picture'}></img>
+      <section>
+        <article>
+          <h2>
+            <a href="#">{user}</a> {message + ' '}
+            {props.link && <a href="#">{props.link}</a>}
+            {unread && <span className="unread"></span>}
+            <time>{time}</time>
+          </h2>
+          {props.picture && <img src={props.picture} alt="picture"></img>}
+        </article>
+        {props.privateMessage && <p>{props.privateMessage}</p>}
+      </section>
+    </li>
+  );
+}
